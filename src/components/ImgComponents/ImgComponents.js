babd3bb1f7c002d62564abb9de6e082d1fa98a6f
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-11 20:04:44 -0800

    img components is done

--- src/components/ImgComponents/ImgComponents.js
+++ src/components/ImgComponents/ImgComponents.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+import React from 'react';
+
+const ImgComponents = (props) => {
+    return(
+        <div>
+            <img src = {props.url} alt = 'NASA daily photo' />
+        </div>
+    )
+}
+
+export default ImgComponents;
