629dd7ab7bd222b27674e4227dee70266996d5e6
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-12 15:29:24 -0800

    img styling is done

--- src/components/ImgComponents/ImgComponents.js
+++ src/components/ImgComponents/ImgComponents.js
@@ -2,11 +2,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ImgDiv = styled.div`
+background-color: black;
+border: 1px solid blue;
+display: flex;
+justify-content: center;
+padding: 2rem;
+margin: 2rem;
+
+`
+
 const ImgComponents = (props) => {
     return(
-        <div>
+        <ImgDiv>
             <img src = {props.url} alt = 'NASA daily photo' />
-        </div>
+        </ImgDiv>
     );
 }
 
