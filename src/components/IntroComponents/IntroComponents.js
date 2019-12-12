85629ed3c84a769508c76d3faed26fd0c617bd4a
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-12 15:21:43 -0800

    intro styling is done

--- src/components/IntroComponents/IntroComponents.js
+++ src/components/IntroComponents/IntroComponents.js
@@ -1,13 +1,21 @@
 import React from "react";
 import styled from 'styled-components';
 
+const WrapperDiv = styled.div`
+color: dodgerblue;
+background: black;
+text-align: center;
+margin: 2rem;
+padding: 1.5rem;
+line-height: 20px;
+`
 const IntroComponents = (props) => {
     return (
-        <div>
+        <WrapperDiv>
             <div className = 'title'>{props.title}</div>
             <div className = 'date'>{props.date}</div>
             <div className = 'explanation'>{props.explanation}</div>
-        </div>
+        </WrapperDiv>
     );
 }
 
